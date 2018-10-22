1106e8cc55045b34201a0b6f21deceea5aad0667
Vanzandtr <rlv220@lehigh.edu>
2018-10-22 14:44:04 -0400

    parse works, added basic search features

--- mtg_project/app/src/main/java/com/example/rlv220/mtg_project/MainActivity.java
+++ mtg_project/app/src/main/java/com/example/rlv220/mtg_project/MainActivity.java
@@ -11,9 +11,13 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
+    ArrayList<JSONObject> cards = new ArrayList<JSONObject>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +26,70 @@ public class MainActivity extends AppCompatActivity {
         tv = findViewById(R.id.test);
         try {
             JSONObject obj = new JSONObject(loadJSONFromAsset(this));
-            //Log.d("tag", "" + obj.names());
-            String[] names = obj.names();
-            JSONArray array = obj.names();
-            for(int i = 0; i < array.length(); i++){
+            //Log.d("Num cards", "" + obj.length());
 
-                JSONObject newobj = obj.getJSONObject(array.getJSONObject(i).toString());
-                Log.d("card", "" + newobj);
-            }
-            String jsonString = obj.toString();
-            String[] split = jsonString.split(",");
+            String card = "Snapcaster Mage";
+         //   getCard(obj, card);
 
-            //JSONObject card = obj.getJSONObject("Adorable Kitten");
-            //String cmc = card.getString("cmc");
-            //String test = obj.getString("name");
-            //tv.setText("" + split[0]);
+            String field = "cmc";
+            String tag = "2";
+            getCardAllCardsWithField(obj, field, tag);
         }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<JSONObject> getAllCards(JSONObject obj) {
+        JSONArray array = obj.names();
+        //Log.d("Array","" + array);
+        JSONObject newobj = new JSONObject();
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                newobj = obj.getJSONObject(array.get(i).toString());
+                //Log.d("card", "" + newobj + "(" + i + ")");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            cards.add(newobj);
+        }
+        return cards;
+    }
+
+    public void getCard(JSONObject obj, String cardName){
+            try {
+                if(obj.get(cardName) != null) {
+                    JSONObject newobj = obj.getJSONObject(cardName);
+                    Log.d("Card", "" + newobj);
+
+                    String name = newobj.getString("name");
+                    Log.d("name","" + name);
+
+                    String manaCost = newobj.getString("manaCost");
+                    Log.d("manaCost","" + manaCost);
+
+                    String cmc = newobj.getString("cmc");
+                    Log.d("cmc","" + cmc);
+
+                    JSONArray subtypes = newobj.getJSONArray("subtypes");
+                    Log.d("subtypes length", "" + subtypes.length());
+                    for(int i = 0;i < subtypes.length(); i++){
+                        Log.d("subtype","" + subtypes.get(i));
+                    }
+                }
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+    public void getCardAllCardsWithField(JSONObject obj, String type, String tag){
+        try {
+            ArrayList<JSONObject> cards = getAllCards(obj);
+            for(int i = 0; i<cards.size(); i++){
+                if (cards.get(i).get(type).equals(Integer.parseInt(tag))){
+                    Log.d("test", "" + cards.get(i));
+                }
+            }
+        }catch(Exception e) {
             e.printStackTrace();
         }
     }
